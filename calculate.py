2c9370fa96035ecbc1aedb050f73f704c5d73fac
agatak8 <shnutzer@gmail.com>
2019-01-06 23:09:22 +0100

    fix

--- calculate.py
+++ calculate.py
@@ -8,24 +8,23 @@ def calculate_times_for_ranges(children_count_range, mutation_range, population_
     logging.basicConfig(filename="cookies-%s.log" % datetime.now(), level=logging.DEBUG)
     print("children;mutation_chance;population_count;elitism_factor;generations_at_end;best_fitness;time")
     for chld_cnt in children_count_range:
+        test_res = generate_tests_results_only(chld_cnt)
         for mut_chnc in mutation_range:
             for pop_cnt in population_range:
                 for elitism in elitism_range:
                     max_gen = 20000
                     starttime = datetime.now()
-                    best_children, best_fitness, generation = calculate(chld_cnt, max_gen, mut_chnc, pop_cnt, elitism)
+                    best_children, best_fitness, generation = calculate(test_res, chld_cnt, max_gen, mut_chnc, pop_cnt, elitism)
                     endtime = datetime.now()
                     print("%d;%d;%d;%d;%d;%d;%s" % (chld_cnt, mut_chnc, pop_cnt, elitism, generation, best_fitness, endtime - starttime))
 
-def calculate(children_count, max_generations, mutation_chance, population_count, elitism_factor):
+def calculate(test_results, children_count, max_generations, mutation_chance, population_count, elitism_factor):
     random.seed(32768)
 
     algo = Algorithm(breeding_method=Algorithm.random_breeding,
                      mutation_method=Algorithm.mutate,
                      crossover_method=Algorithm.equal_crossover,
                      selection_method=Algorithm.select_k_best)
-
-    test_results = generate_tests_results_only(children_count)
 
     # # time start
     # tstart = datetime.now()
@@ -51,7 +50,9 @@ if __name__ == '__main__':
 
     children_count, max_generations, mutation_chance, population_count, elitism_factor = (int(x) for x in sys.argv[1:])
 
-    calculate(children_count, max_generations, mutation_chance, population_count, elitism_factor)
+    test_results = generate_tests_results_only(children_count)
+
+    calculate(test_results, children_count, max_generations, mutation_chance, population_count, elitism_factor)
     # # time end
     # tend = datetime.now()
     # print(str(tend - tstart))
