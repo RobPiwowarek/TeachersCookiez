7943aa407cc4deefd1d615a93421a0479a06225f
agatak8 <shnutzer@gmail.com>
2019-01-08 20:29:24 +0100

    new params

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import random
 
 if __name__ == '__main__':
     random.seed(None)
-    calculate_times_for_ranges([50], [0, 20, 50], [50, 75, 100, 125, 150], [2, 4, 16])
+    calculate_times_for_ranges([50], [0, 10, 20, 30, 40, 50], [50, 75, 100, 125, 150], [2, 4, 8])
